596a7332bac8fc48f40db1d3788821a0ada9a741
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-10-20 23:21:08 -0300

    Trying to test if conditional in Scala

---
+++ DPK04-Simple-Pattern-Matcher/scala/IfSimplePatternMatcher.scala
@@ -0,0 +1,9 @@
+import scala.io.StdIn.readLine
+
+object ConsoleInputExample:
+    def consoleInput(args: Array[String]): Unit =
+        println ("Please enter the country")
+        val country = readLine()
+         
+    def SimpleMatcher2(args: String): Unit =
+        if 
