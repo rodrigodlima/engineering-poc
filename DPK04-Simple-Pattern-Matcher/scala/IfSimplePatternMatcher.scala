5614eacf1c3091e2d9f67794caeae3f77098fa8c
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-10-21 23:05:53 -0300

    Create example com if else conditional

--- DPK04-Simple-Pattern-Matcher/scala/IfSimplePatternMatcher.scala
+++ DPK04-Simple-Pattern-Matcher/scala/IfSimplePatternMatcher.scala
@@ -1,9 +1,20 @@
 import scala.io.StdIn.readLine
 
-object ConsoleInputExample:
-    def consoleInput(args: Array[String]): Unit =
-        println ("Please enter the country")
-        val country = readLine()
-         
-    def SimpleMatcher2(args: String): Unit =
-        if 
+def consoleInput(country: String): Unit =
+  if country == "Usa" then
+    println("Language is English")
+  else if country == "Spain" then
+    println("Language is Spanish")
+  else if country == "Italy" then
+    println("Language is Italian")
+  else if country == "France" then
+    println("Language is French")
+  else if country == "Germany" then
+    println("Language is German")
+  else
+    println("No valid value")
+    
+@main def run(): Unit =
+  println("Please enter the country")
+  val country = readLine()
+  consoleInput(country)
