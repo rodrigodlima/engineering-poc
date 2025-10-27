077c742dbd9ecc633c75f04dfcac18def71ffc4f
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-10-26 23:29:28 -0300

    Remove old codes

--- DPK02-Revert-List/scala/Main.scala
+++
@@ -1,5 +0,0 @@
-@main
-
-def reverseList(): Unit = 
-    val list = List(1,2,3,4,5)
-    println(list.reverse.mkString(","))
--- List/list.scala
+++
@@ -1,7 +0,0 @@
-@main
-
-def ListLetters(): Unit =
-    val list = List("a", "b", "c") 
-    println(list(0)) //print a
-    println(list(1)) //print b
-    println(list(2)) // print c
