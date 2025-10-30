c75ffd772a7c60058eb49afb7d757abd32aa66da
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-10-30 00:00:18 -0300

    Update java

---
+++ DPK01-Revert-String/java/src/DPK01_impl_2.java
@@ -0,0 +1,19 @@
+public class DPK01_impl_2 {
+
+    public static String reverseString(String str) {
+        String reversed = "";
+        var i = str.length() - 1;
+        while (i<=0) {
+            
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        String original = "Hello";
+        String reversed = reverseString(original);
+
+        System.out.println(reversed);
+    }
+}
