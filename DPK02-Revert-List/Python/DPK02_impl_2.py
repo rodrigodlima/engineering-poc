247705830c62ffa4958493af6721b2ec5d52025c
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-01-01 23:58:32 -0300

    Refactor revert_list function for clarity and efficiency

--- DPK02-Revert-List/Python/DPK02_impl_2.py
+++ DPK02-Revert-List/Python/DPK02_impl_2.py
@@ -1,8 +1,8 @@
 def revert_list(list):
-    len_string = len(list)
-    for len_string in list:
-        list_reverse = list_reverse + list[len_string - 1]
-        len_string = len_string - 1
+    list_reverse = []
+    len_list = len(list)
+    for i in range(len_list):
+        list_reverse.append(list[len_list - 1 - i])
     return list_reverse
 
 values = [1, 2, 3, 4, 5]
