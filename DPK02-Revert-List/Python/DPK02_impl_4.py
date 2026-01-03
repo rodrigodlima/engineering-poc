de414d9175153a7431bded21d7ed335182b6ebd7
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-01-03 19:20:45 -0300

    Add implementation of revert_list function using if

---
+++ DPK02-Revert-List/Python/DPK02_impl_4.py
@@ -0,0 +1,15 @@
+def revert_list(lst):
+    result = []
+    i = len(lst) - 1
+
+    while True:
+        if i < 0:
+            break
+        result.append(lst[i])
+        i -= 1
+
+    return result
+
+
+values = [1, 2, 3, 4, 5]
+print(revert_list(values)) 
