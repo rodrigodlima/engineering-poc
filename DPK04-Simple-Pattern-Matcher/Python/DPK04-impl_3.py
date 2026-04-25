3a24ba6569d0310581fd0ad40d69c0ddf66cc8c5
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-04-24 23:18:17 -0300

    Add initial implementation of pattern matcher with country-language mapping

---
+++ DPK04-Simple-Pattern-Matcher/Python/DPK04-impl_3.py
@@ -0,0 +1,16 @@
+COUNTRIES = [
+    ("Usa", "English"),
+    ("Brasil", "Portuguese"),
+    ("Spain", "Spanish"),
+    ("Italy", "Italian"),
+    ("France", "French"),
+    ("Germany", "German")
+]
+
+
+
+def pattern_matcher(country: str) -> str:
+    return next(filter(lambda x: x[0] == country, COUNTRIES))[1]
+
+
+print(pattern_matcher("Brasil"))
